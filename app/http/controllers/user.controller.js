58fa4e7e238c54c94bf5f92fd78bf088ed3961c3
Hamid <hamid.mohammadi.ir@gmail.com>
2022-07-27 09:39:16 +0430

    list of user invites

--- app/http/controllers/user.controller.js
+++ app/http/controllers/user.controller.js
@@ -64,6 +64,20 @@ class UserController {
 		}
 	}
 
+	async getAllUserRequests(req, res, next) {
+		try {
+			const userId = req.user._id;
+			const { invite_requests } = await UserModel.findById(userId, { invite_requests: 1 });
+			return res.status(200).json({
+				status: 200,
+				success: true,
+				requests: invite_requests || [],
+			});
+		} catch (error) {
+			next(error);
+		}
+	}
+
 	addSkills() {}
 
 	updateSkills() {}
